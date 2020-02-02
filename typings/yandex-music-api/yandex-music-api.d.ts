a1eb00009053ffb4f161d272c83beec456ed8737
Cherkashin Alexander <cherkalexander@gmail.com>
2020-02-02 12:22:14 +0300

    Fix typings for YandexMusicApi

--- typings/yandex-music-api/yandex-music-api.d.ts
+++ typings/yandex-music-api/yandex-music-api.d.ts
@@ -2,24 +2,15 @@ declare module "yandex-music-api" {
   class YandexMusicApi {
     constructor();
 
-    init(config: { username: string; password: string }): Promise<InitResult>;
-    getFeed(): Promise<FeedResult>;
+    init(config: { username: string; password: string }): Promise<YandexMusicApi.InitResult>;
+    getFeed(): Promise<YandexMusicApi.FeedResult>;
     getUserPlaylists(userId: string): any;
     getPlaylist(userId: string, playListId: string): any;
-    getPlaylists(
-      userId: string,
-      playlists: string[],
-      options: { mixed: string; "rich-tracks": string }
-    ): any[];
-    createPlaylist(name: string, options: { visibility: Visibility }): any;
+    getPlaylists(userId: string, playlists: string[], options: { mixed: string; "rich-tracks": string }): any[];
+    createPlaylist(name: string, options: { visibility: YandexMusicApi.Visibility }): any;
     removePlaylist(playlistId: string): any;
     renamePlaylist(playlistId: string, newName: string);
-    addTracksToPlaylist(
-      playlistId: string,
-      tracks: Array<{ id: string; albumId: string }>,
-      revision: string,
-      options: { at: number }
-    ): any;
+    addTracksToPlaylist(playlistId: string, tracks: Array<{ id: string; albumId: string }>, revision: string, options: { at: number }): any;
     removeTracksFromPlaylist(
       playlistId: string,
       tracks: Array<{ id: string; albumId: string }>,
@@ -28,70 +19,72 @@ declare module "yandex-music-api" {
     ): any;
   }
 
-  interface InitResult {
-    access_token: string;
-    expires_in: number;
-    token_type: string;
-    uid: number;
-  }
-
-  interface FeedResult {
-    canGetMoreEvents: boolean;
-    days: any[];
-    generatedPlaylist: GeneratedPlayListItem[];
-    headlines: any[];
-    isWizardPassed: boolean;
-    pumpkin: boolean;
-    today: string;
-  }
-
-  interface GeneratedPlayListItem {
-    data: {
-      animatedCoverUri: string;
-      available: boolean;
-      collective: boolean;
-      cover: Cover;
-      coverWithoutText: Cover;
-      created: string;
-      description: string;
-      descriptionFormatted: string;
-      durationMs: number;
-      everPlayed: boolean;
-      generatedPlaylistType: string; //"playlistOfTheDay";
-      idForFrom: string; //"playlist_of_the_day";
-      isBanner: boolean;
-      isPremiere: boolean;
-      kind: number;
-      madeFor: any;
-      modified: string;
-      ogImage: string;
-      ogTitle: string;
-      owner: any;
-      playCounter: any;
-      prerells: any[];
-      revision: number;
-      snapshot: number;
-      tags: any[];
-      title: string;
-      trackCount: number;
-      tracks: any[];
+  namespace YandexMusicApi {
+    interface InitResult {
+      access_token: string;
+      expires_in: number;
+      token_type: string;
       uid: number;
-      urlPart: string;
-      visibility: Visibility;
-    };
-    notify: boolean;
-    ready: boolean;
-    type: "playlistOfTheDay";
-  }
+    }
+    
+    interface FeedResult {
+      canGetMoreEvents: boolean;
+      days: any[];
+      generatedPlaylist: GeneratedPlayListItem[];
+      headlines: any[];
+      isWizardPassed: boolean;
+      pumpkin: boolean;
+      today: string;
+    }
 
-  type Visibility = "public" | "private";
+    interface GeneratedPlayListItem {
+      data: {
+        animatedCoverUri: string;
+        available: boolean;
+        collective: boolean;
+        cover: Cover;
+        coverWithoutText: Cover;
+        created: string;
+        description: string;
+        descriptionFormatted: string;
+        durationMs: number;
+        everPlayed: boolean;
+        generatedPlaylistType: string; //"playlistOfTheDay";
+        idForFrom: string; //"playlist_of_the_day";
+        isBanner: boolean;
+        isPremiere: boolean;
+        kind: number;
+        madeFor: any;
+        modified: string;
+        ogImage: string;
+        ogTitle: string;
+        owner: any;
+        playCounter: any;
+        prerells: any[];
+        revision: number;
+        snapshot: number;
+        tags: any[];
+        title: string;
+        trackCount: number;
+        tracks: any[];
+        uid: number;
+        urlPart: string;
+        visibility: Visibility;
+      };
+      notify: boolean;
+      ready: boolean;
+      type: "playlistOfTheDay";
+    }
 
-  interface Cover {
-    custom: boolean;
-    dir: string;
-    type: string;
-    uri: string;
-    version: string;
+    type Visibility = "public" | "private";
+
+    interface Cover {
+      custom: boolean;
+      dir: string;
+      type: string;
+      uri: string;
+      version: string;
+    }
   }
 
   export = YandexMusicApi;
