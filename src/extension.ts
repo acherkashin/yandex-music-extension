19d0ef0befc4638e750de0f5ef6290d2ef4535c6
acherkashin <cherkalexander@gmail.com>
2020-03-14 23:21:56 +0300

    Add rewind setting

--- src/extension.ts
+++ src/extension.ts
@@ -48,15 +48,20 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand("yandexMusic.rewindForward", () => {
       // move 15 to the settings
-      store.rewind(15);
+      store.rewind(getRewindTime());
     })
   );
 
   context.subscriptions.push(
     vscode.commands.registerCommand("yandexMusic.rewindBackward", () => {
-      store.rewind(-15);
+      store.rewind(-getRewindTime());
     })
   );
+}
+
+function getRewindTime(): number {
+  const defaultValue = vscode.workspace.getConfiguration("yandexMusic").inspect<number>("rewindTime")?.defaultValue;
+  return vscode.workspace.getConfiguration("yandexMusic").get<number>("rewindTime") || defaultValue || 15;
 }
 
 // this method is called when your extension is deactivated
