b39283d98826f83c23cd8b78f9d2db72df9205e5
acherkashin <cherkalexander@gmail.com>
2020-04-05 12:26:55 +0300

    add getPlayListIcon

--- src/tree/treeItems/playListTreeItem.ts
+++ src/tree/treeItems/playListTreeItem.ts
@@ -1,29 +1,11 @@
 import * as vscode from "vscode";
-import { getThemeIcon, getResourceIcon } from "../../utils";
+import { getPlayListIcon } from "../../utils";
 import { PlayList } from "../../yandexApi/playlist/playList";
-import { GeneratedPlayList } from "../../yandexApi/feed/generatedPlayList";
 
 export class PlayListTreeItem extends vscode.TreeItem {
   constructor(public readonly playList: PlayList) {
     super(playList.title, vscode.TreeItemCollapsibleState.Collapsed);
 
-    if ("generatedPlaylistType" in playList) {
-      const playListType = (playList as GeneratedPlayList).generatedPlaylistType;
-      if (playListType === "playlistOfTheDay") {
-        this.iconPath = getResourceIcon("playLists/playlistOfTheDay.png");
-      } else if (playListType === "recentTracks") {
-        this.iconPath = getResourceIcon("playLists/recentTracks.png");
-      } else if (playListType === "neverHeard") {
-        this.iconPath = getResourceIcon("playLists/neverHeard.png");
-      } else if (playListType === "podcasts") {
-        this.iconPath = getResourceIcon("playLists/podcasts.png");
-      } else if (playListType === "missedLikes") {
-        this.iconPath = getResourceIcon("playLists/missedLikes.png");
-      } else if (playListType === "origin") {
-        this.iconPath = getResourceIcon("playLists/origin.png");
-      }
-    } else {
-      this.iconPath = getThemeIcon("playlist.svg");
-    }
+    this.iconPath = getPlayListIcon(playList);
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
 import * as path from "path";
 import { ThemeIcon } from "vscode";
+import { PlayList } from "./yandexApi/playlist/playList";
+import { GeneratedPlayList } from "./yandexApi/feed/generatedPlayList";
 
 export function getThemeIcon(iconFileName: string): ThemeIcon {
     return {
@@ -10,4 +12,18 @@ export function getThemeIcon(iconFileName: string): ThemeIcon {
 
 export function getResourceIcon(iconFileName: string): string {
     return path.join(__dirname, "..", "resources", iconFileName);
+}
+
+const generatedPlayListTypes = ["playlistOfTheDay", "recentTracks", "neverHeard", "podcasts", "missedLikes", "origin"];
+
+export function getPlayListIcon(playList: PlayList) {
+    if ("generatedPlaylistType" in playList) {
+        const playListType = (playList as GeneratedPlayList).generatedPlaylistType;
+
+        if (generatedPlayListTypes.indexOf(playListType) !== -1) {
+            return getResourceIcon(`playLists/${playListType}.png`);
+        }
+    }
+
+    return getThemeIcon("playlist.svg");
 }
