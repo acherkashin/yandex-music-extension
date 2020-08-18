477f1ca6edd8c1ab2c871cccc61424085e5e47fd
acherkashin <cherkalexander@gmail.com>
2020-08-18 22:07:19 +0300

    cannot dislike chart tracks

--- src/tree/treeItems/chartTreeItem.ts
+++ src/tree/treeItems/chartTreeItem.ts
@@ -1,7 +1,5 @@
 import * as vscode from "vscode";
-import { Track } from "../../yandexApi/interfaces";
 import { getArtists } from "../../yandexApi/apiUtils";
-import { getThemeIcon } from "../../utils/iconUtils";
 import { ChartItem } from "../../yandexApi/landing/chartitem";
 import { TrackTreeItem } from "../treeItems/trackTreeItem";
 import { Store } from "../../store";
@@ -11,14 +9,5 @@ export class ChartTreeItem extends TrackTreeItem {
     super(store, item.track, vscode.TreeItemCollapsibleState.None);
 
     this.label = `${item.chart.position}. ${item.track.title} - ${getArtists(item.track)}`;
-    this.command = {
-      command: "yandexMusic.play",
-      title: "Play Track",
-      tooltip: `Play ${this.label}`,
-      arguments: [this],
-    };
-
-    this.iconPath = getThemeIcon("track.svg");
-    this.contextValue = "track";
   }
 }
