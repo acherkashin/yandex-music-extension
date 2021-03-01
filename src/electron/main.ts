7d9cb17863d3d35ed4f969983da5bff384cd12d3
Cherkashin Alexander <cherkalexander@gmail.com>
2021-03-01 21:58:14 +0300

    #28 Electron icon is shown in Dock on macOs

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -39,6 +39,9 @@ function createWindow() {
     });
 }
 
+// Hide electrom icon from dock on macOs
+app.dock?.hide();
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
