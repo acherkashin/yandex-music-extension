b71f56a9e7f9cbca705f300a06229474bd224a11
Cherkashin Alexander <cherkalexander@gmail.com>
2023-03-01 22:53:11 +0300

    Fix: Actions missing when rename playlist

--- src/tree/treeItems/playListTreeItem.ts
+++ src/tree/treeItems/playListTreeItem.ts
@@ -7,22 +7,23 @@ export class PlayListTreeItem extends vscode.TreeItem {
   constructor(public playList: Playlist) {
     super('');
     this.update(playList);
+    // contextValue should be initialized just ones in constructor, 
+    // otherwise actions in context menu will not be available when contextValue is re-initialized 
+    this.contextValue = 'playlist';
+    this.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
   }
-
+  
   update(playList: Playlist) {
     this.playList = playList;
     this.label = playList.title;
     this.description = playList.description;
     this.tooltip = [playList.title, playList.description].join('. ');
-    this.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
-
+    
     if (playList.cover?.uri) {
       const uri = getCoverUri(playList.cover.uri, 50);
       this.iconPath = vscode.Uri.parse(uri);
     } else {
       this.iconPath = getPlayListIcon(playList);
     }
-
-    this.contextValue = 'playlist';
   }
 }
